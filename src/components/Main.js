362f0e7fdc491c3deea480ed11c20c46942af67a
Anasemos Kassahun <Anasemosaka@gmail.com>
2022-06-17 14:29:24 -0400

    finished auth

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,13 +8,21 @@ import Show from '../pages/Show';
 
 
 const Main = (props) => {
-
     const [ people, setPeople ] = useState(null);
 
     const URL = 'http://localhost:4000/people/';
 
     const getPeople = async () => {
-        const response = await fetch(URL);
+        if(!props.user) return;
+        // JSON Web Token AKA 
+        const token = await props.user.getIdToken();
+        
+        const response = await fetch(URL, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token
+            }
+        });
         const data = await response.json();
         setPeople(data);
     }
@@ -22,10 +30,12 @@ const Main = (props) => {
 
     const createPeople = async (person) => {
             if(!props.user) return;  // if there is no user return out of the function
+            const token = await props.user.getIdToken();
             await fetch(URL, {
                 method: 'POST',
                 headers: {
-                    'Content-type': 'Application/json'
+                    'Content-type': 'Application/json',
+                    'Authorization': 'Bearer ' + token
                 },
                 body: JSON.stringify(person)
             });
@@ -35,10 +45,14 @@ const Main = (props) => {
 
     const updatePeople = async (updatedPerson, id) => {
         if(!props.user) return;
+        const token = await props.user.getIdToken();
+
             await fetch(URL + id, {
                 method: "PUT",
                 headers: {
-                    'Content-type': 'Application/json'
+                    'Content-type': 'Application/json',
+                    'Authorization': 'Bearer ' + token
+
                 },
                 body: JSON.stringify(updatedPerson)
             });
@@ -47,11 +61,16 @@ const Main = (props) => {
 
     const deletePeople = async (id) => {
         if(!props.user) return;
+        const token = await props.user.getIdToken();
 
         // confirm deletion operations go here
         // if yes
             await fetch(URL + id, {
-                method: "DELETE"
+                method: "DELETE",
+                headers: {
+                    'Authorization': 'Bearer ' + token
+                },
+
             });
             getPeople();
             // if no
@@ -62,8 +81,13 @@ const Main = (props) => {
 
 
     useEffect(() => {
+            if(props.user) {
+                getPeople();
+            } else {
+                setPeople(null);
+            }
             getPeople();
-    }, []);
+    }, [props.user]);
 
     return (
         <main>
